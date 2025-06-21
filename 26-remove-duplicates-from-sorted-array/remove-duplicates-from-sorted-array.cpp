52ee7afa11e2856b5f9a76bf2f422196726f67ae
riteshfageriya <167625034+riteshfageriya@users.noreply.github.com>
2025-06-21 14:43:29 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (20.53%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        set<int> st;
+    //     set<int> st;
 
-        int n=nums.size();
-       for(int i=0;i<n;i++)
-       {
-             st.insert(nums[i]);
-        }
-        int j=0;
-        for(auto it:st){
-            nums[j]=it;
-            j++;
-        }
-        return j;
+    //     int n=nums.size();
+    //    for(int i=0;i<n;i++)
+    //    {
+    //          st.insert(nums[i]);
+    //     }
+    //     int j=0;
+    //     for(auto it:st){
+    //         nums[j]=it;
+    //         j++;
+    //     }
+    //     return j;
+     // in this the space complexity is o(n) and time complexitiy is n +nlogn
+ // optimal solution 
+    int i=0;
+    int n=nums.size();
+    for(int j=1;j<n;j++){
+    if(nums[j]!=nums[i]){
+     nums[i+1]=nums[j] ;
+    i++;}
+    
+    }
+    return i+1;
     }
 };
+ // in this the space complexity is o(n) and time complexitiy is n +nlogn
+ // optimal 
+ 
+  
